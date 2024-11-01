67ac90f0927f30a45f10c2a60396a00892fd75e2
UbaiddRaza <ubaiddraza@gmail.com>
2024-11-01 16:00:36 +0500

    firebase added

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,31 @@ import { useState, useRef } from 'react';
 function App() {
   const [todo, setTodo] = useState([]);
   const input = useRef(null);
-  const editInput = useRef(null);
 
   const addTodo = (event) => {
     event.preventDefault();
     console.log(input.current.value);
-    todo.push({ title: input.current.value, id: Date.now() });
-    setTodo([...todo]);
+    const newTodo = { title: input.current.value, id: Date.now() };
+    setTodo([...todo, newTodo]);
     console.log(todo);
     input.current.value = "";
   };
 
   const deleteTodo = (index) => {
     console.log('todo deleted', index);
-    todo.splice(index, 1);
-    setTodo([...todo]);
+    setTodo(todo.filter((item, i) => i !== index));
   };
 
   const editTodo = (index) => {
     const newTitle = prompt("Enter new title:");
     if (newTitle !== null) {
-      todo[index].title = newTitle;
-      setTodo([...todo]);
+      const updatedTodo = todo.map((item, i) => {
+        if (i === index) {
+          return { ...item, title: newTitle };
+        }
+        return item;
+      });
+      setTodo(updatedTodo);
     }
   };
 
@@ -44,7 +47,7 @@ function App() {
               <button onClick={() => editTodo(index)}>Edit</button>
             </li>
           );
-        }) : <h1></h1>}
+        }) : <h3>Todo List empty</h3>}
       </ol>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layout.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProtectedRoutes from './components/ProtectedRoutes.jsx'
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <ProtectedRoutes component={<Home />}/>
+      },
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'register',
+        element: <Register />
+      },
+      
+       
+        ]
+      }
+    ]
+  
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
+
+
+
+
+// firebase authentication
+// protected routes
+// nested routes
